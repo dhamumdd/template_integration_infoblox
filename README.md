7214cc545102a29eae3e6ef9c4849f1c6da34bc4
CAMHub PublishBot <octravis@us.ibm.com>
2018-03-13 22:04:10 +0000

    Automated CAMHub::publish on 2018-03-13 22:04:10 UTC

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# template_integration_infoblox
+# Infoblox Integration Module
+
+This repo contains a Module to interact with an Infoblox server to get and return a IP address from the IPAM service.
+[Infoblox](https://www.infoblox.com) is an industry leader in DNS, DHCP, and IP address management
+
+## License
+
+This code is released under the Apache 2.0 License. Please see [LICENSE](https://github.com/CAMHub-Open-Development/template_integration_infoblox) for more details.
+
+Copyright IBM Corp. 2018
---
+++ other/terraform/scripts/get_ip.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# =================================================================
+# Copyright 2018 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#	  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =================================================================
+
+# set -x
+
+# Get script parameters
+while test $# -gt 0; do
+  [[ $1 =~ ^-u|--user ]] && { PARAM_USER="${2}"; shift 2; continue; };
+  [[ $1 =~ ^-p|--password ]] && { PARAM_PASSWORD="${2}"; shift 2; continue; };
+  [[ $1 =~ ^-i|--ipamip ]] && { PARAM_IPAMIP="${2}"; shift 2; continue; };
+  [[ $1 =~ ^-h|--hostname ]] && { PARAM_HOSTNAME="${2}"; shift 2; continue; };
+  [[ $1 =~ ^-n|--network ]] && { PARAM_NETWORK="${2}"; shift 2; continue; };
+  break;
+done
+
+json_network="{\"network\":\"${PARAM_NETWORK}\""
+json_hostname="{\"name\":\"${PARAM_HOSTNAME}\""
+#echo $json_network
+#echo $json_hostname
+
+json="${json_hostname},\"ipv4addrs\":[{\"ipv4addr\":{\"_object_function\":\"next_available_ip\",\"_result_field\":\"ips\",\"_object\""
+json2=" : \"network\",\"_object_parameters\":${json_network}}}}]}"
+
+
+full_json=${json}${json2}
+#echo $full_json
+
+curerespo=`curl -k \
+                 -u ${PARAM_USER}:${PARAM_PASSWORD}  \
+                 -H 'content-type: application/json' \
+                 -X POST "https://${PARAM_IPAMIP}/wapi/v2.6.1/record:host?_return_fields%2B=name,ipv4addrs&_return_as_object=1" \
+                 -d "${full_json}"`
+
+
+address=`echo $curerespo | jq -r '.result .ipv4addrs[0].ipv4addr'`
+host=`echo $curerespo | jq -r '.result .ipv4addrs[0].host'`
+short_host=`echo $host |  cut -d'.' -f 1`
+domain=`echo $host |  cut -d'.' -f 2-`
+
+#echo $address
+#echo $host
+#echo $short_host
+#echo $domain
+
+return_result="{\"ipaddress\": \"${address}\", \"hostname\": \"${short_host}\",\"domain\": \"${domain}\"}"
+
+echo $return_result
